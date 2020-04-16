1d5c4b8a900c875e467ef024a2b3a9776c4e0178
vkshay3400 <akshayp2208@gmail.com>
2020-04-16 17:03:37 +0530

    Added: Exception type

--- src/main/java/com/bridgelabz/parkinglot/clientcode/ParkingLotException.java
+++ src/main/java/com/bridgelabz/parkinglot/clientcode/ParkingLotException.java
@@ -4,8 +4,8 @@ public class ParkingLotException extends Exception {
     private final String message;
     private final ExceptionType type;
 
-    public enum ExceptionType{
-        PARKING_LOT_FULL;
+    public enum ExceptionType {
+        PARKING_LOT_FULL, PARKING_LOT_EMPTY;
     }
 
     public ParkingLotException(ExceptionType type, String message) {
