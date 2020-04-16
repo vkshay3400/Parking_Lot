b97eb941128ad01593ff58af930b7bc414edb89f
vkshay3400 <akshayp2208@gmail.com>
2020-04-16 17:06:10 +0530

    Added: Method for parked and unparked vehicle

--- src/main/java/com/bridgelabz/parkinglot/clientcode/ParkingLot.java
+++ src/main/java/com/bridgelabz/parkinglot/clientcode/ParkingLot.java
@@ -2,26 +2,37 @@ package com.bridgelabz.parkinglot.clientcode;
 
 public class ParkingLot {
     // VARIABLE
-    private Object vehicle;
+    private Object vehicle = null;
 
     // CONSTRUCTOR
     public ParkingLot() {
     }
 
-    // METHOD FOR PARK
-    public boolean park(Object vehicle) throws ParkingLotException {
+    // METHOD FOR PARKING LOT
+    public void park(Object vehicle) throws ParkingLotException {
         if (this.vehicle != null)
-            throw new ParkingLotException(ParkingLotException.ExceptionType.PARKING_LOT_FULL,"Parking is full");
+            throw new ParkingLotException(ParkingLotException.ExceptionType.PARKING_LOT_FULL, "Parking is full");
         this.vehicle = vehicle;
+    }
+
+    // METHOD FOR UNPARKING LOT
+    public void unPark(Object vehicle) throws ParkingLotException {
+        if (this.vehicle == null)
+            throw new ParkingLotException(ParkingLotException.ExceptionType.PARKING_LOT_EMPTY, "Parking is empty");
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+        }
+    }
+
+    // METHOD FOR PARKED VEHICLE
+    public boolean isVehicleParked(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) return true;
         return false;
     }
 
-    // METHOD FOR UNPARK
-    public boolean unPark(Object vehicle){
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
-            return true;
-        }
+    // METHOD FOR UNPARKED VEHICLE
+    public boolean isVehicleUnparked(Object vehicle) {
+        if (this.vehicle != vehicle) return true;
         return false;
     }
 }
