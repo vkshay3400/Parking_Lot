dbd382c1b25ee68b12015e0c0ae573566bbe5bb5
vkshay3400 <akshayp2208@gmail.com>
2020-04-19 17:18:40 +0530

    Added: Test cases for owner and security

--- src/test/java/com/bridgelabz/parkinglot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglot/ParkingLotTest.java
@@ -1,26 +1,34 @@
 package com.bridgelabz.parkinglot;
 
 import com.bridgelabz.parkinglot.clientcode.ParkingLot;
-import com.bridgelabz.parkinglot.clientcode.ParkingLotException;
+import com.bridgelabz.parkinglot.vehicledetails.VehicleDetails;
+import com.bridgelabz.parkinglot.exception.ParkingLotException;
+import com.bridgelabz.parkinglot.parkingnotification.OwnerNotification;
+import com.bridgelabz.parkinglot.parkingnotification.SecurityNotification;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
     ParkingLot parkingLot = null;
-    Object vehicle = null;
+    VehicleDetails vehicleDetails = null;
+    private OwnerNotification owner;
+    private SecurityNotification airportSecurity;
 
     @Before
     public void setUp() throws Exception {
-        vehicle = new Object();
         parkingLot = new ParkingLot();
+        owner = new OwnerNotification();
+        airportSecurity = new SecurityNotification();
+
     }
 
     @Test
-    public void givenOwnersVehicle_WhenParked_ShouldReturnTrue() {
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
         try {
-            parkingLot.park(vehicle);
-            boolean isParked = parkingLot.isVehicleParked(vehicle);
+            VehicleDetails vehicleDetails = new VehicleDetails("MH01AB1234", "WagonR");
+            parkingLot.park(vehicleDetails);
+            boolean isParked = parkingLot.isVehicleParked(vehicleDetails);
             Assert.assertTrue(isParked);
         } catch (ParkingLotException e) {
             e.printStackTrace();
@@ -28,25 +36,47 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenOwnersVehicle_WhenUnParked_ShouldReturnTrue() {
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
         try {
-            parkingLot.park(vehicle);
-            parkingLot.unPark(vehicle);
-            boolean isUnparked = parkingLot.isVehicleUnparked(vehicle);
+            VehicleDetails vehicleDetails = new VehicleDetails("MH01AB1234", "WagonR");
+            parkingLot.park(vehicleDetails);
+            parkingLot.unPark(vehicleDetails);
+            boolean isUnparked = parkingLot.isVehicleUnparked(vehicleDetails);
             Assert.assertTrue(isUnparked);
         } catch (ParkingLotException e) {
-            Assert.assertEquals(ParkingLotException.ExceptionType.PARKING_LOT_EMPTY, e.getMessage());
+            Assert.assertEquals(ParkingLotException.ExceptionType.NO_VEHICLE, e.getMessage());
         }
     }
 
     @Test
-    public void givenOwnersVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
         try {
-            parkingLot.park(vehicle);
-            boolean isParked = parkingLot.isVehicleUnparked(vehicle);
+            VehicleDetails vehicleDetails = new VehicleDetails("MH01AB1234", "WagonR");
+            parkingLot.park(vehicleDetails);
+            boolean isParked = parkingLot.isVehicleUnparked(vehicleDetails);
             Assert.assertFalse(isParked);
         } catch (ParkingLotException e) {
             Assert.assertEquals(ParkingLotException.ExceptionType.PARKING_LOT_FULL, e.getMessage());
         }
+    }
+
+    @Test
+    public void givenAVehicle_WhenParkingLotIsFull_ShouldInformToOwner() throws ParkingLotException {
+        parkingLot.addObserver(owner);
+        VehicleDetails vehicleDetails1 = new VehicleDetails("MH01AB1234", "WagonR");
+        parkingLot.park(vehicleDetails1);
+        VehicleDetails vehicleDetails2 = new VehicleDetails("MH01AB2345", "Santro");
+        parkingLot.park(vehicleDetails2);
+        Assert.assertEquals("Parking lot is full", owner.getParkingStatus());
+    }
+
+    @Test
+    public void givenAVehicle_WhenParkingLotIsFull_ShouldInformToAirportSecurity() throws ParkingLotException {
+        parkingLot.addObserver(airportSecurity);
+        VehicleDetails vehicleDetails1 = new VehicleDetails("MH01AB1234", "WagonR");
+        parkingLot.park(vehicleDetails1);
+        VehicleDetails vehicleDetails2 = new VehicleDetails("MH01AB2345", "Santro");
+        parkingLot.park(vehicleDetails2);
+        Assert.assertEquals("Parking lot is full", airportSecurity.getParkingStatus());
     }
 }
